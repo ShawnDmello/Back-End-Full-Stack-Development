f3e64e7014fc8bb0432bf4bab9e0fa06a5a5e190
Shawn D'mello <lbshawn9045@gmail.com>
2025-11-13 18:58:28 +0000

    Update and rename lessons.js to Class.js

---
+++ models/Class.js
@@ -0,0 +1,13 @@
+export let classes = [
+  {
+    id: 1,
+    title: "Mathematics Advanced",
+    description: "Algebra and Calculus",
+    price: 45,
+    image: "images/maths.jpg",
+    availableInventory: 10,
+    rating: 5,
+    category: "Maths",
+    location: "London"
+  }
+];
