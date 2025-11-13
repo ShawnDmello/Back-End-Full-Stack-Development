75355b5ebb97463fc80c05bfa3a332669f57225e
Shawn D'mello <lbshawn9045@gmail.com>
2025-11-13 18:51:47 +0000

    Add classRoutes for handling class data

---
+++ routes/classRoutes.js
@@ -0,0 +1,18 @@
+import express from "express";
+import { classes } from "../data/classes.js";
+
+const router = express.Router();
+
+// GET all classes
+router.get("/", (req, res) => {
+  res.json(classes);
+});
+
+// ADD new class
+router.post("/", (req, res) => {
+  const newClass = { id: Date.now(), ...req.body };
+  classes.push(newClass);
+  res.json(newClass);
+});
+
+export default router;
